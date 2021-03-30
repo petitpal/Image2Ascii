e8d6bdc5e8a7efe8a9be03cc11cee52c79de39b0
Paul Newman <paul.newman@teamsoftware.com>
2021-03-30 14:33:38 +0100

    Adding some new unit tests

--- Image2Ascii.Services.Test/ChunkServiceTests.cs
+++ Image2Ascii.Services.Test/ChunkServiceTests.cs
@@ -2,6 +2,7 @@ using Img2Asc.Services;
 using Moq;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace Image2Ascii.Test
 {
@@ -20,13 +21,13 @@ namespace Image2Ascii.Test
 
         private static IEnumerable<TestCaseData> ChunkCalculation_TestData()
         {
-            // chunk width / image width / expected chunks
-            yield return new TestCaseData(20, 200, 10).SetName("{m}(10/200)");
-            yield return new TestCaseData(20, 400, 20).SetName("{m}(20/400)");
-            yield return new TestCaseData(20, 399, 20).SetName("{m}(20/399)");
-            yield return new TestCaseData(1, 100, 100).SetName("{m}(1/100)");
-            yield return new TestCaseData(49, 100, 3).SetName("{m}(49/100)");
-            yield return new TestCaseData(32, 1024, 32).SetName("{m}(32/1024)");
+            // chunk dimension / image width / expected chunks
+            yield return new TestCaseData(20, 200, 10).SetName("{m} (10/200)");
+            yield return new TestCaseData(20, 400, 20).SetName("{m} (20/400)");
+            yield return new TestCaseData(20, 399, 20).SetName("{m} (20/399)");
+            yield return new TestCaseData(1, 100, 100).SetName("{m} (1/100)");
+            yield return new TestCaseData(49, 100, 3).SetName("{m} (49/100)");
+            yield return new TestCaseData(32, 1024, 32).SetName("{m} (32/1024)");
         }
 
 
@@ -53,6 +54,36 @@ namespace Image2Ascii.Test
 
             // assert
             Assert.AreEqual(expectedChunks, chunkCount);
+        }
+
+
+
+        private static IEnumerable<TestCaseData> GetChunks_Basic_TestDate()
+        {
+            // bitmap width / bitmap height / chunk width / chunk height / expected chunks wide / expected chunks high
+            yield return new TestCaseData(100, 100, 10, 10, 10, 10).SetName("{m} (100/100/10/10/10/10)");
+            yield return new TestCaseData(100, 200, 10, 10, 10, 20).SetName("{m} (100/200/10/10/10/20)");
+        }
+
+
+        [TestCaseSource(nameof(GetChunks_Basic_TestDate))]
+        public void GetChunks_Basic(
+            int bitmapWidth,
+            int bitmapHeight,
+            int chunkWidth,
+            int chunkHeight,
+            int expectedChunkWidth,
+            int expectedChunkHeight)
+        {
+            // arrange
+            var source = new Bitmap(bitmapWidth, bitmapHeight);
+
+            // act
+            var chunks = _chunkService.GetChunks(source, chunkWidth, chunkHeight);
+
+            // assert
+            Assert.AreEqual(expectedChunkWidth, chunks.GetLength(1));
+            Assert.AreEqual(expectedChunkHeight, chunks.GetLength(0));
         }
 
     }
